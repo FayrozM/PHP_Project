4685818b9b8f704cae7ad8e64c8e8b7b929633f2
Fayroz Moadi <fayrozmoadi2001@gmail.com>
2022-01-08 18:26:51 +0200

    Update login.php
    separate css and php

--- finalProject/backend/login.php
+++ finalProject/backend/login.php
@@ -49,39 +49,10 @@ session_start();
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" href="../frontend/login.css">
 	<title>Login</title>
 </head>
 <body>
-
-	<style type="text/css">
-	
-	#text{
-
-		height: 25px;
-		border-radius: 5px;
-		padding: 4px;
-		border: solid thin #aaa;
-		width: 100%;
-	}
-
-	#button{
-
-		padding: 10px;
-		width: 100px;
-		color: white;
-		background-color: lightblue;
-		border: none;
-	}
-
-	#box{
-
-		background-color: grey;
-		margin: auto;
-		width: 300px;
-		padding: 20px;
-	}
-
-	</style>
 
 	<div id="box">
 		
