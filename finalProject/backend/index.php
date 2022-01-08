332e7f7ddfd69a518cf392756d6c25ee124bb362
Fayroz Moadi <fayrozmoadi2001@gmail.com>
2022-01-08 18:59:20 +0200

    Update index.php

--- finalProject/backend/index.php
+++ finalProject/backend/index.php
@@ -12,14 +12,30 @@ session_start();
 <html>
 <head>
 <link rel="stylesheet" href="../frontend/index.css">
-	<title>My website</title>
+	<title>home</title>
 </head>
 <body>
+<div>
+	<div class="Navbar">
+		<a href="index.html" id="logo"><span>V</span>ubert</a>
+			<!--ID - של סרגל הכלים -->
+			<ul id="Toolbar">
+			<li><a href="index.html">Home</a></li>
+			<li><a href="bundles.html">Bundles</a></li>
+			<li><a href="community.html">Community</a></li>
+			<li><a href="contact.html">Contact</a></li>
+			<li><a href="about.html">About</a></li>
+			<li>|</li>
+			<li><a href="signIn.html">Sign in</a></li>
+			</ul>
+	</div>
 
-	<a href="logout.php">Logout</a>
-	<h1>This is the index page</h1>
+		<a href="logout.php">Logout</a>
+		<h1>This is the index page</h1>
 
-	<br>
-	Hello, <?php echo $user_data['user_name']; ?>
+		<br>
+		Hello, <?php echo $user_data['user_name']; ?>
+</div>
+
 </body>
 </html>
