00392df5db09235dddb013335781adc8a02de51b
Fayroz Moadi <fayrozmoadi2001@gmail.com>
2022-01-08 20:11:05 +0200

    Update index.php
    add search

--- finalProject/backend/index.php
+++ finalProject/backend/index.php
@@ -18,22 +18,19 @@ session_start();
 <div class="NavBar">
 <a href="index.html" id="logo"><span>F</span>un maker</a>
     <ul>  
-        <li><a class="act" href="home.php">Home</a></li>
-        <li><a href="flights.html">Flights </a></li>
-        <li><a href="hotel.html"> Hotel </a></li>
-        <li><a href="car.html"> Car Rentals </a></li>
-        <li><a href="attractions.html"> Attractions </a></li>
-        <li><a href="signup.php" > Sign in </a></li>
-		<li><a href="logout.php">Logout</a></li>
-        
+        <li><a href="flights.html">Home </a></li>
+		<li><a href="attractions.html"> About us </a></li>
+        <li><a href="hotel.html"> Favorite list </a></li>
+        <li><a href="car.html"> contact us</a></li>
+		<input id="search"  placeholder="Search category ..." >
+		<input id="search"  placeholder="Search attraction ...">
+
     </ul>
 </div>
 		
 <br>
 <br><br><br><br><br><br><br>
 	<div class="main">
-	<a> href="logout.php">Logout</a>
-		<br>
 		Hello, <?php echo $user_data['user_name']; ?>
 	</div>
 
