62a0c42ea7a1a0d77d21909181312aaf26d22a25
Fayroz Moadi <fayrozmoadi2001@gmail.com>
2022-01-11 19:57:00 +0200

    Update index.php
    cart

--- finalProject/backend/index.php
+++ finalProject/backend/index.php
@@ -1,103 +1,161 @@
 <?php 
 session_start();
+$connect = mysqli_connect("localhost", "root", "", "login_sample_db");
 
-	include("connection.php");
-	include("functions.php");
+if(isset($_POST["add_to_cart"]))
+{
+	if(isset($_SESSION["shopping_cart"]))
+	{
+		$item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
+		if(!in_array($_GET["id"], $item_array_id))
+		{
+			$connect = count($_SESSION["shopping_cart"]);
+			$item_array = array(
+				'item_id'			=>	$_GET["id"],
+				'item_name'			=>	$_POST["hidden_name"],
+				'item_price'		=>	$_POST["hidden_price"],
+				'item_quantity'		=>	$_POST["quantity"]
+			);
+			$_SESSION["shopping_cart"][$count] = $item_array;
+		}
+		else
+		{
+			echo '<script>alert("Item Already Added")</script>';
+		}
+	}
+	else
+	{
+		$item_array = array(
+			'item_id'			=>	$_GET["id"],
+			'item_name'			=>	$_POST["hidden_name"],
+			'item_price'		=>	$_POST["hidden_price"],
+			'item_quantity'		=>	$_POST["quantity"]
+		);
+		$_SESSION["shopping_cart"][0] = $item_array;
+	}
+}
 
-	$user_data = check_login($con);
+if(isset($_GET["action"]))
+{
+	if($_GET["action"] == "delete")
+	{
+		foreach($_SESSION["shopping_cart"] as $keys => $values)
+		{
+			if($values["item_id"] == $_GET["id"])
+			{
+				unset($_SESSION["shopping_cart"][$keys]);
+				echo '<script>alert("Item Removed")</script>';
+				echo '<script>window.location="index.php"</script>';
+			}
+		}
+	}
+}
 
 ?>
-
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="stylesheet" href="../frontend/style.css">
-	<title>home</title>
-</head>
-<body>
-	<div class="main">
-	<div class="NavBar">
-<a href="index.php" id="logo"><span>F</span>un maker</a>
-<span class="NavBar1" ></span>
-<ul>  
-<li><a href="cart.php"> cart</a></li>
-		<li><a href="admins.php"> About us </a></li>
-        <li><a href="#"> Favorite list </a></li>
-        <li><a href="feedback.php">Feedback</a></li>
-</ul>
-</span>
-	<ul class="navnav">
-		<input id="search"  placeholder="Search category ..." >
-		<input id="search"  placeholder="Search attraction ...">
-		<li><span> welcome back , <?php echo $user_data['user_name']; ?> </span></li>
-		<li><a href="logout.php">Logout</a></li>
-    </ul>
-</div>
+	<head>
+		<title>Webslesson Demo | Simple PHP Mysql Shopping Cart</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+	</head>
+	<body>
+		<br />
+		<div class="container">
+			<br />
+			<br />
+			<br />
+			<br /><br />
+			<?php
+				$query = "SELECT * FROM tbl_product ORDER BY id ASC";
+				$result = mysqli_query($connect, $query);
+				if(mysqli_num_rows($result) > 0)
+				{
+					while($row = mysqli_fetch_array($result))
+					{
+				?>
+			<div class="col-md-4">
+				<form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>">
+					<div style="border:1px solid #333; background-color:#f1f1f1; border-radius:5px; padding:16px;" align="center">
+						<img src="images/<?php echo $row["image"]; ?>" class="img-responsive" /><br />
 
-<br><br><br><br><br><br><br><br><br><br>
+						<h4 class="text-info"><?php echo $row["name"]; ?></h4>
 
-	<div class="hi">
-	<span>To make your weekend better :) 
-	<br><br><br>
-	The israel is home to some of the most spectacular scenery in the world <br> 
-	and some of the most recognizable icons on the planet. <br>
-				Many of the top attractions in israel are bucket list destinations, drawing visitors from around the world.<br>
+						<h4 class="text-danger">$ <?php echo $row["price"]; ?></h4>
 
-				Plenty of options await you, with incredible sights sprinkled across the country. 
-				<br>
-				<br>
-				--> Note: Some businesses may be temporarily closed due to recent global health and safety issues - ( Covid-19 ).
-	</span>
-</div>
+						<input type="text" name="quantity" value="1" class="form-control" />
 
+						<input type="hidden" name="hidden_name" value="<?php echo $row["name"]; ?>" />
 
+						<input type="hidden" name="hidden_price" value="<?php echo $row["price"]; ?>" />
 
+						<input type="submit" name="add_to_cart" style="margin-top:5px;" class="btn btn-success" value="Add to Cart" />
 
-	<br><br><br><br><br><br>
-
-<div class="allCategorys">
-	<div class="category">
-			<div class="card" style="width: 18rem;">
-			<img class="card-img-top" src="../images/Indoor category.png" alt="Card image cap">
-			<div class="card-body">
-				<p class="card-text"><a href="indoorCategory.php">Indoor category</p>
+					</div>
+				</form>
 			</div>
+			<?php
+					}
+				}
+			?>
+			<div style="clear:both"></div>
+			<br />
+			<h3>Order Details</h3>
+			<div class="table-responsive">
+				<table class="table table-bordered">
+					<tr>
+						<th width="40%">Item Name</th>
+						<th width="10%">Quantity</th>
+						<th width="20%">Price</th>
+						<th width="15%">Total</th>
+						<th width="5%">Action</th>
+					</tr>
+					<?php
+					if(!empty($_SESSION["shopping_cart"]))
+					{
+						$total = 0;
+						foreach($_SESSION["shopping_cart"] as $keys => $values)
+						{
+					?>
+					<tr>
+						<td><?php echo $values["item_name"]; ?></td>
+						<td><?php echo $values["item_quantity"]; ?></td>
+						<td>$ <?php echo $values["item_price"]; ?></td>
+						<td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2);?></td>
+						<td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
+					</tr>
+					<?php
+							$total = $total + ($values["item_quantity"] * $values["item_price"]);
+						}
+					?>
+					<tr>
+						<td colspan="3" align="right">Total</td>
+						<td align="right">$ <?php echo number_format($total, 2); ?></td>
+						<td></td>
+					</tr>
+					<?php
+					}
+					?>
+						
+				</table>
 			</div>
+		</div>
 	</div>
-	<div class="category">
-			<div class="card" style="width: 18rem;">
-			<img class="card-img-top" src="../images/Extreme category.png" alt="Card image cap">
-			<div class="card-body">
-				<p class="card-text"><a href="extremeCategory.php">Extreme category</p>
-			</div>
-			</div>
-	</div>
-	<div class="category">
-			<div class="card" style="width: 18rem;">
-			<img class="card-img-top" src="../images/Water category.png" alt="Card image cap">
-			<div class="card-body">
-				<p class="card-text"><a href="waterCategory.php">Water category</p>
-			</div>
-			</div>
-	</div>
-	<div class="category">
-			<div class="card" style="width: 18rem;">
-			<img class="card-img-top" src="../images/Offroad category.png" alt="Card image cap">
-			<div class="card-body">
-				<p class="card-text"><a href="offroadCategory.php">Offroad category</p>
-			</div>
-			</div>
-	</div>
-	<div class="category">
-			<div class="card" style="width: 18rem;">
-			<img class="card-img-top" src="../images/Ski category.png" alt="Card image cap">
-			<div class="card-body">
-				<p class="card-text"><a href="winterCategory.php">Winter category</p>
-			</div>
-			</div>
-	</div>
-</div>
-
-	</div>
-</body>
+	<br />
+	</body>
 </html>
+
+<?php
+//If you have use Older PHP Version, Please Uncomment this function for removing error 
+
+/*function array_column($array, $column_name)
+{
+	$output = array();
+	foreach($array as $keys => $values)
+	{
+		$output[] = $values[$column_name];
+	}
+	return $output;
+}*/
+?>
